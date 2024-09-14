f60ace13640018d68d158b2002e80f775cca75d9
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-09-14 16:34:08 +0600

    how it work page workign and testimonials page done

---
+++ src/Pages/How it Work/HowItWork/HowItWork.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GoodFood from '../../Home/why Simple Food/GoodFood';
+
+const HowItWork = () => {
+     return (
+          <div>
+              <GoodFood></GoodFood>
+          </div>
+     );
+};
+
+export default HowItWork;
--- src/Pages/Testimonials/Testimonials.jsx
+++ src/Pages/Testimonials/Testimonials.jsx
@@ -1,14 +1,58 @@
 import React from 'react'
 
 export default function Testimonials() {
-  return (
-    <div>
-      <div>
-          <h1 className='text-5xl font-semibold'> Testimonials </h1>
-      </div>
-       <div>
-          
-       </div>
-    </div>
-  )
+     return (
+          <div>
+             <div className='container '>
+             <div>
+                    <h1 className='text-5xl text-start font-semibold'> Testimonials </h1>
+               </div>
+               <div className='mt-6 bg-gray-50 lg:pl-24 lg:p-9 rounded-xl'>
+                    <div className='flex lg:gap-12 gap-5 flex-col lg:flex-row'>
+                         <img className='lg:size-64' src="https://i.ibb.co.com/HDpHC8W/Ellipse-14.png" alt="" />
+                         <div className='text-start '>
+                              <h1 className='text-2xl'> Anjelina</h1>
+                              <h1 className='text-xl text-yellow-600 py-4'> Student </h1>
+                              <p className='text-lg'> elit nibh elit lacus vitae lacus sed diam dui. est. lacus hendrerit enim. non Nam sollicitudin. tincidunt ex urna. lacus, elit. tincidunt Nunc lorem. adipiscing vitae hendrerit Nunc ipsum est. est. felis, dui non elit. sit at, Sed non nec
+
+                                   viverra fringilla placerat in nisl. tincidunt amet, nec non, risus urna ac ex. Nullam Donec nec tortor. Morbi ex urna. placerat odio leo. nulla, lobortis, faucibus risus at, vitae Donec Quisque tincidunt libero, sapien laoreet Quisque id
+
+                              </p>
+
+                         </div>
+                    </div>
+               </div>
+               <div className='mt-6  lg:pl-24 lg:p-9'>
+                    <div className='flex lg:gap-12 gap-5 flex-col lg:flex-row-reverse'>
+                         <img className='lg:size-64' src="https://i.ibb.co.com/g9wZWmh/Ellipse-14-1.png" alt="" />
+                         <div className='text-start '>
+                              <h1 className='text-2xl'> Anjelina</h1>
+                              <h1 className='text-xl text-yellow-600 py-4'> Student </h1>
+                              <p className='text-lg'> elit nibh elit lacus vitae lacus sed diam dui. est. lacus hendrerit enim. non Nam sollicitudin. tincidunt ex urna. lacus, elit. tincidunt Nunc lorem. adipiscing vitae hendrerit Nunc ipsum est. est. felis, dui non elit. sit at, Sed non nec
+
+                                   viverra fringilla placerat in nisl. tincidunt amet, nec non, risus urna ac ex. Nullam Donec nec tortor. Morbi ex urna. placerat odio leo. nulla, lobortis, faucibus risus at, vitae Donec Quisque tincidunt libero, sapien laoreet Quisque id
+
+                              </p>
+
+                         </div>
+                    </div>
+               </div>
+               <div className='mt-6 bg-gray-50 lg:pl-24 lg:p-9 rounded-xl'>
+                    <div className='flex lg:gap-12 gap-5 flex-col lg:flex-row'>
+                         <img className='lg:size-64' src="https://i.ibb.co.com/HDpHC8W/Ellipse-14.png" alt="" />
+                         <div className='text-start '>
+                              <h1 className='text-2xl'> Anjelina</h1>
+                              <h1 className='text-xl text-yellow-600 py-4'> Student </h1>
+                              <p className='text-lg'> elit nibh elit lacus vitae lacus sed diam dui. est. lacus hendrerit enim. non Nam sollicitudin. tincidunt ex urna. lacus, elit. tincidunt Nunc lorem. adipiscing vitae hendrerit Nunc ipsum est. est. felis, dui non elit. sit at, Sed non nec
+
+                                   viverra fringilla placerat in nisl. tincidunt amet, nec non, risus urna ac ex. Nullam Donec nec tortor. Morbi ex urna. placerat odio leo. nulla, lobortis, faucibus risus at, vitae Donec Quisque tincidunt libero, sapien laoreet Quisque id
+
+                              </p>
+
+                         </div>
+                    </div>
+               </div>
+             </div>
+          </div>
+     )
 }
